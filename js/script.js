aa5c14827dfb920e994fa82bb39329482fdb8926
Aron Singel <108942904+Shrekcoding@users.noreply.github.com>
2025-04-13 22:52:46 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let menus = document.querySelector("nav");
+let menuBtn = document.querySelector(".menu-btn");
+let closeBTN = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click",function(){
+menus.classList.add("active");
+
+
+
+})
+
+closeBTN.addEventListener("click",function(){
+
+    menus.classList.remove("active");
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
